431b1c5bd47236c71937fa240df8ed90942fd2bb
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-12-13 16:37:45 +0700

    initiilaize commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Home from './pages/Home';
+import Post from './pages/Post';
+import User from './pages/User';
+import Navbar from "./layouts/Navbar";
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/post" element={<Post/>} />
+          <Route path="/user" element={<User/>} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
