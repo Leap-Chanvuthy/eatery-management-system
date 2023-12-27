d070c3d0b3996f16e917a9577362bbb3166ee6a9
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2023-12-28 01:02:30 +0700

    add: new feed page

--- src/components/Newfeeds/Feed.jsx
+++ src/components/Newfeeds/Feed.jsx
@@ -1,26 +1,12 @@
-import { RxHamburgerMenu } from "react-icons/rx";
-import { CiCirclePlus } from "react-icons/ci";
-import { FaUsersViewfinder } from "react-icons/fa6";
-import { FaHeart } from "react-icons/fa6";
-import { FaRegCommentDots } from "react-icons/fa";
-import { useToggleContext } from "../../contexts/ToggleContext";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import { CiTimer } from "react-icons/ci";
 import { database } from "../../configs/firebase";
 import {collection , getDoc, getDocs} from 'firebase/firestore';
 
 
 
 const Feed = () => {
-    const {toggleEffect} = useToggleContext();
-    const [love , setLove] = useState(false);
-    const handleClick = () => {
-        setLove(!love);
-    }
-
-    // blog post state
-    const [post , setPost] = useState([]);
+    const [posts , setPosts] = useState([]);
     const [loading , setLoading] = useState(true);
     const postCollection = collection(database , 'posts')
     useEffect(() => {
@@ -31,7 +17,7 @@ const Feed = () => {
                 snapshot.forEach((doc) => {
                     newPost.push({id: doc.id , ...doc.data() });
                 })
-                setPost(newPost);
+                setPosts(newPost);
                 setLoading(false);
                 console.log(newPost);
             }
@@ -45,65 +31,37 @@ const Feed = () => {
     
 
     return ( 
-        <div className="w-full h-[80vh] shadow-md bg-base-200 overflow-y-scroll rounded-lg">
-            <div className="m-2 flex justify-between rounded-md p-2">
-                <div className="flex justify-center gap-2 text-black">
-                    <Link to="/post" className="flex justify-center bg-yellow-600 font-bold items-center gap-1 px-3 py-1 rounded-lg">
-                       <CiCirclePlus className="font-bold"/>
-                       <p className="text-sm">New Post</p> 
-                    </Link>
-                    {/* <Link className="flex justify-center bg-red-500 font-bold items-center gap-1 px-3 py-1 rounded-lg">
-                       <FaUsersViewfinder className="font-bold"/>
-                       <p className="text-sm">Suggestion</p> 
-                    </Link> */}
-                </div>
-                <button onClick={toggleEffect}>
-                    <RxHamburgerMenu/>
-                </button>
-            </div>
-            <hr />
-            {/* Newfeed area */}
 
-            {post && post.map((p) => (
-            <div className="w-full h-[10.2rem] bg-base-300 my-1" key={p.id}>
-                {/* <img
-                    src="https://wwwnc.cdc.gov/travel/images/mountain-biker.jpg"
-                    className="w-full h-full object-cover p-1 rounded-md"
-                    alt="Responsive Image"
-                /> */}
-                <div className="">
-                    <div className="h-[5rem] overflow-y-scroll pl-2" >
-                        <p className="font-light text-sm">{p.article}</p>
+        <div className="mx-auto mt-10 grid  max-w-2xl grid-cols-1 gap-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article key={post.id} className="flex max-w-xl bg-gray-800 rounded-sm flex-col items-start justify-between">
+              <div className="group relative">
+                <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg" className="w-full" />
+                <div className="flex justify-between items-center px-4 py-2">
+                    <h3 className=" text-lg font-semibold  text-gray-200 ">title</h3>
+                    <div className="flex justify-center items-center gap-2">
+                        <CiTimer className="text-gray-200"/>
+                        <time dateTime={post.createdAt} className="text-gray-200 text-xs">date</time>
                     </div>
-                    <div className="bg-base-300 p-2 lg:flex md:block items-center gap-6 w-full h-[5rem] ">
-                            <div className="flex gap-3 items-center">
-                                <div className="mask mask-squircle w-8 h-8 avatar">
-                                    <img src="https://th-i.thgim.com/public/incoming/9uvnty/article67222424.ece/alternates/BASE_SQUARE/2023-08-04T094328Z_1224629970_RC2WG2AC6YU7_RTRMADP_3_WPP-RESULTS-TWITTER.JPG" alt="Avatar Tailwind CSS Component" />
-                                </div>
-                                <p className="font-bold text-sm text-gray-400">Leap Chanvuthy</p>
-                            </div>
-                            <div className="flex justify-start pl-11 gap-6">
-                                <div className="flex justify-center gap-2 items-center">
-                                    <button onClick={handleClick}><FaHeart className={`${love? 'text-red-500' : 'text-white-500' } `} /></button>
-                                    <p className="text-sm">{p.like} Likes</p>
-                                </div>
-                                <div className="flex justify-center gap-2 items-center">
-                                    <FaRegCommentDots/>
-                                    <p className="text-sm">{p.comment.length} Comments</p>
-                                </div>
-                                <div className="flex justify-center gap-2 items-center">
-                                    <p className="text-sm text-green-500"></p>
-                                </div>
-                            </div>
-                    </div>
-                    <hr />
                 </div>
-                
-            </div>
-            ))}
-
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-500 px-4">{post.article}</p>
+              </div>
+              <div className="relative mt-8 px-4 py-2 flex items-center gap-x-4">
+                <img src='https://example.com' className="h-10 w-10 rounded-full bg-gray-50" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-200">
+                    <a href='https://example.com'>
+                      <span className="absolute inset-0" />
+                      author name
+                    </a>
+                  </p>
+                  <p className="text-gray-500">Frontend Developer</p>
+                </div>
+              </div>
+            </article>
+          ))}
         </div>
-     );
+    );
 }
  
 export default Feed;
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,51 +1,152 @@
-import {Link} from 'react-router-dom';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
-    return (
-        <div className="navbar bg-base-200 shadow-xs shadow-black">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">T-Book</a>
-            </div>
-            <div className="flex-none">
-                <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
-                    </svg>
-                    <span className="badge badge-sm indicator-item text-red-400">8</span>
-                    </div>
-                </div>
-                <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div className="card-body">
-                    <span className="font-bold text-lg">8 Items</span>
-                    <span className="text-info">Subtotal: $999</span>
-                    <div className="card-actions">
-                        <button className="btn btn-primary btn-block">View cart</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                    <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                    <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                    </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
-                </div>
-            </div>
-        </div>
-    );
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Post', href: '/post', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
- 
-export default Navbar;
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        to={item.href}
+                        key={item.name}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
