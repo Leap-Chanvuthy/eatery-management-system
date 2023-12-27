6ef3bf41711356ce6f576688a9c4e429570fdbc7
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2023-12-28 01:23:41 +0700

    add: Blog post form

--- src/components/Newfeeds/Post.jsx
+++ src/components/Newfeeds/Post.jsx
@@ -1,20 +1,104 @@
-const Post = () => {
-    return ( 
-        
+import { useState } from 'react';
 
-<form class="max-w-md mx-auto pt-7  p-5">
-  <div class="relative z-0 w-full mb-5 group">
-      <input type="file" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-      <label for="floating_email" class="peer-focus:font-medium absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-  </div>
-  <div class="relative z-0 w-full mb-5 group">
-      <textarea type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-      <label for="floating_password" class="peer-focus:font-medium absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">What's on your mind ?</label>
-  </div>
-  <button type="submit" class="bg-yellow-600 px-6 py-1 text-black font-bold rounded-sm text-md focus:ring-4 focus:outline-none">Post</button>
-</form>
+const BlogPostForm = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    article: '',
+    images: [],
+    category: '',
+  });
 
-     );
-}
- 
-export default Post;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const imagesArray = Array.from(e.target.files);
+    setFormData({
+      ...formData,
+      images: imagesArray,
+    });
+  };
+
+  return (
+    <div className="max-w-lg mx-auto p-6 bg-white">
+      <h2 className="text-2xl font-bold mb-4">Post a Blog</h2>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Enter the title of your blog"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="article" className="block text-sm font-medium text-gray-700">
+            Article
+          </label>
+          <textarea
+            id="article"
+            name="article"
+            value={formData.article}
+            onChange={handleChange}
+            rows="5"
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Write your blog post here..."
+          ></textarea>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="images" className="block text-sm font-medium text-gray-700">
+            Images
+          </label>
+          <input
+            type="file"
+            id="images"
+            name="images"
+            accept="image/*"
+            multiple
+            onChange={handleImageChange}
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="category" className="block text-sm font-medium text-gray-700">
+            Category
+          </label>
+          <input
+            type="text"
+            id="category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Enter the category of your blog"
+          />
+        </div>
+
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:border-indigo-300"
+          >
+            Post Blog
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default BlogPostForm;
